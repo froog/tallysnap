c66218a29b76f5293e6e95343ec48e76b1bbbc14
Daniel Pickering <daniel.pickering@gmail.com>
2026-03-17 22:35:48 +1300

    Add index.html and main.jsx for Vite dev server

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CardCount from './cardcount.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CardCount />
+  </React.StrictMode>
+);
